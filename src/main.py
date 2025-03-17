a654b01b08816af7b8c0974971ef8cf2fe337292
saisneha26 <palle.saisneha@gmail.com>
2025-03-17 14:02:48 -0500

    updated port config

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from src.database import Base, engine
 from src.api import router
+import uvicorn
+import os
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,3 +12,7 @@ app = FastAPI(
     version="0.1",
 )
 app.include_router(router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Use Azure's dynamic port
+    uvicorn.run(app, host="0.0.0.0", port=port)
